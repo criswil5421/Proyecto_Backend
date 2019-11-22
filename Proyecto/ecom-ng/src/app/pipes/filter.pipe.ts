937aabe03355cd7c2d0490f4ecdcfbdeb40ee5f6
Annderson <ronald.lopez@upeu.edu.pe>
2019-11-21 19:21:03 -0500

    busquedaestesi

--- Proyecto/ecom-ng/src/app/pipes/filter.pipe.ts
+++ Proyecto/ecom-ng/src/app/pipes/filter.pipe.ts
@@ -8,11 +8,15 @@ export class FilterPipe implements PipeTransform {
   transform(value: any, ...arg: any[]): any {
     const resultPost= [];
     for(const post of value){
-      if(post.nombreempresa.indexOf(arg)> -1){
+      if(post.nombreempresa.toLowerCase().indexOf(arg)> -1){
         resultPost.push(post)
-        };
+      };
+      if(post.descripcion.toLowerCase().indexOf(arg)> -1){
+        resultPost.push(post)
+      };
     };
     return resultPost;
   }
+  
 
 }
